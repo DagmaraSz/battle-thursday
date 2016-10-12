6fce952a85d7e5e83ccb3631560391b126c314df
Royston <rlqhenson@hotmail.com>
2016-10-12 14:58:38 +0100

    added hit points feature test and code

--- spec/features/hit_point_spec.rb
+++ spec/features/hit_point_spec.rb
@@ -1,14 +1,14 @@
 require "././app"
 require "spec_helper"
 
-RSpec.feature "player name management", :type => :feature do
-  scenario "adding player names at start of game" do
+RSpec.feature "hit point management", :type => :feature do
+  scenario "see Player 2's hitpoints" do
     visit "/"
 
     fill_in "Player 1", :with => "Dagmara"
     fill_in "Player 2", :with => "Royston"
     click_button "Fight!"
 
-    expect(page).to have_text("Dagmara Vs Royston")
+    expect(page).to have_text("Royston's HP : 100/100")
   end
 end
